8211a778f9ac6e8f422ba9db8bb6c2ca95773579
Amsiam <amsiam990@gmail.com>
2021-10-15 00:42:59 +0600

    linked list

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,7 +1,33 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
-    
+typedef struct Node
+{
+    int data;
+    Node *next;
+    Node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+} Node;
+
+Node *createLinkedList(Node *head, int val)
+{
+}
+
+void printList(Node *node)
+{
+    while (node != NULL)
+    {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = NULL;
 }
