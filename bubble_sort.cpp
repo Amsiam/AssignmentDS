22f5ab16aaf50877f59e7256245308dd4d0224ba
Amsiam <amsiam990@gmail.com>
2021-10-18 23:30:06 +0600

    bubble sort in string

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,41 +1,39 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-
-void arrayInput(int arr[], int n){
-    for(int i=0;i<n;i++)cin>>arr[i];
-}
-
-void print(int arr[],int n){
-    for(int i=0;i<n;i++)cout<<arr[i]<<" ";
-    cout<<endl;
-}
-
-void bubbleSort(int arr[],int n){
-    int t;
-    for(int i=0;i<n-1;i++){
-        for (int j = 0; j < n-i-1; j++)
+void bubbleSort(string &arr, int n)
+{
+    char t;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
         {
-            if(arr[j]>arr[j+1]){
-                t = arr[j+1];
-                arr[j+1] = arr[j];
+            if (arr[j] > arr[j + 1])
+            {
+                t = arr[j + 1];
+                arr[j + 1] = arr[j];
                 arr[j] = t;
             }
         }
-        
     }
 }
 
-int main(){
-    cout<<"Enter Size of Array: ";
+int main()
+{
+    cout << "Enter Size of Array: ";
     int n;
-    cin>>n;
-    int arr[n];
-    cout<<"Enter Elements of the array: "<<endl;
-    arrayInput(arr,n);
-    bubbleSort(arr,n);
-    cout<<"Sorted Array"<<endl;
-    print(arr,n);
+    cin >> n;
+    string arr;
+    cout << "Enter String: " << endl;
+    cin >> arr;
+    bubbleSort(arr, n);
+    cout << "Sorted Array" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
